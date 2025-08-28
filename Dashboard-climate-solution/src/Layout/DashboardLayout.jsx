232e64e73caf7a69277958bc640b1ee3ae8cccec
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-08-28 11:20:37 +0200

    done

--- Dashboard-climate-solution/src/Layout/DashboardLayout.jsx
+++ Dashboard-climate-solution/src/Layout/DashboardLayout.jsx
@@ -6,6 +6,11 @@ import Sidebar from '../Components/Sidebar';
 
 
 const DashboardLayout = () => {
+    const handleLogout = () => {
+        // clear login state
+        localStorage.removeItem("isLoggedIn"); // if using localStorage
+        window.location.href = "/login";       // redirect to login
+    };
 
     const location = useLocation();
 
@@ -22,10 +27,19 @@ const DashboardLayout = () => {
                         <Sidebar />
 
                         {/* User Info */}
-                        <div className="mt-auto flex items-center gap-2">
-                            <User className="w-8 h-8 text-gray-600" />
-                            <p>Jack</p>
+                        <div className="mt-auto flex justify-between items-center gap-2">
+                            <div className="flex items-center gap-2">
+                                <User className="w-8 h-8 text-gray-600" />
+                                <p className='font-bold'>Jack</p>
+                            </div>
+                            <button
+                                onClick={handleLogout}
+                                className="text-red-600 hover:text-red-800 text-sm mt-2 font-bold border-2 rounded-sm px-2 py-1"
+                            >
+                                Logout
+                            </button>
                         </div>
+
                     </div>
                 )}
 
